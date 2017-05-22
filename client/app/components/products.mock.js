bb64cdcae3c5f395d0c5ec68877c2082ad402ca4
Jakub Zwirko <jakub.zwirko@btfinancialservices.com>
2017-05-22 15:53:58 +1000

    removed inlined svgs

--- client/app/components/products.mock.js
+++ client/app/components/products.mock.js
@@ -1,17 +1,17 @@
 export default {
   products: [
-    { id: 1, name: 'Avocado', price: 20, image: 'avocado' },
-    { id: 2, name: 'Chocolate', price: 21, image: 'chocolate' },
-    { id: 3, name: 'Coffee', price: 22, image: 'coffee' },
-    { id: 4, name: 'Apple', price: 20.3, image: 'apple' },
-    { id: 5, name: 'Kiwi', price: 21, image: 'kiwi' },
-    { id: 6, name: 'Mango', price: 22, image: 'mango' },
-    { id: 7, name: 'Melon', price: 20.4, image: 'melon' },
-    { id: 8, name: 'Orage', price: 21, image: 'orange' },
-    { id: 9, name: 'Rapsberry', price: 22, image: 'rapsberry' },
-    { id: 10, name: 'Strawberry', price: 20.5, image: 'strawberry' },
-    { id: 11, name: 'Vanilla', price: 20.6, image: 'vanilla' },
-    { id: 12, name: 'Watermelon', price: 20.7, image: 'watermelon' }
+    { id: 1, name: 'Avocado', price: 20, image: 'avocado' , imgUrl: 'https://kubek80.github.io/assets/avocado.png'},
+    { id: 2, name: 'Chocolate', price: 21, image: 'chocolate', imgUrl: 'https://kubek80.github.io/assets/chocolate-strawberry.png'},
+    { id: 3, name: 'Coffee', price: 22, image: 'coffee', imgUrl: 'https://kubek80.github.io/assets/frosted-coffee.png' },
+    { id: 4, name: 'Apple', price: 20.3, image: 'apple', imgUrl: 'https://kubek80.github.io/assets/green-apple.png' },
+    { id: 5, name: 'Kiwi', price: 21, image: 'kiwi', imgUrl: 'https://kubek80.github.io/assets/kiwi-banana.png' },
+    { id: 6, name: 'Mango', price: 22, image: 'mango' , imgUrl: 'https://kubek80.github.io/assets/mango-banana.png' },
+    { id: 7, name: 'Melon', price: 20.4, image: 'melon' , imgUrl: 'https://kubek80.github.io/assets/melon-mint.png' },
+    { id: 8, name: 'Orage', price: 21, image: 'orange', imgUrl: 'https://kubek80.github.io/assets/orange-carrot.png' },
+    { id: 9, name: 'Rapsberry', price: 22, image: 'rapsberry', imgUrl: 'https://kubek80.github.io/assets/rapsberry-banana.png' },
+    { id: 10, name: 'Strawberry', price: 20.5, image: 'strawberry', imgUrl: 'https://kubek80.github.io/assets/strawberry.png' },
+    { id: 11, name: 'Vanilla', price: 20.6, image: 'vanilla', imgUrl: 'https://kubek80.github.io/assets/vanilla.png' },
+    { id: 12, name: 'Watermelon', price: 20.7, image: 'watermelon', imgUrl: 'https://kubek80.github.io/assets/watermelon.png' }
   ],
   couriers: [
     { id: 1, name: 'Toll', price: 100},
